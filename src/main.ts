7962963f67fec7b707d1f2a4d032111e82bd96cc
DeNic0la <fine.stick4800@denic0la.ch>
2026-03-11 15:32:36 +0100

    add service worker support and manifest for PWA functionality

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
+import { isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideServiceWorker } from '@angular/service-worker';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers ?? []),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
+}).catch((err) => console.error(err));
