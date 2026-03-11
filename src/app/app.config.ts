b67a3fbf748239c64f048520d38f25e3f5beb471
DeNic0la <fine.stick4800@denic0la.ch>
2026-03-11 14:24:41 +0100

    add Statsig integration with session replay and web analytics plugins

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,24 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { STATSIG_INIT_CONFIG } from '@statsig/angular-bindings';
+import { StatsigSessionReplayPlugin } from '@statsig/session-replay';
+import { StatsigAutoCapturePlugin } from '@statsig/web-analytics';
 import { routes } from './app.routes';
+
+
+const StatsigConfig = {
+  sdkKey: "client-GmvqYVSPkAuEhQg5ktIz8QH1bQ4cucBg2JG23g0DIMJ",
+  user: {userID: 'annonymous'}, // initial user object
+  options: {disableCompression: true,plugins: [StatsigSessionReplayPlugin, StatsigAutoCapturePlugin]}
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    {
+      provide: STATSIG_INIT_CONFIG,
+      useValue: StatsigConfig,
+    },
   ]
 };
